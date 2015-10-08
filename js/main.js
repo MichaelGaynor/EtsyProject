9569d3dd5a5605154f12072e3eb4ccb7a29f5be7
Michael Gaynor <jmichaelgaynor@gmail.com>
2015-10-08 14:07:37 -0400

    Attempting js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 (function () {
 
-  console.log('It Works!');
+  // console.log('It Works!');
+  // console.log($);
+  // console.log(_);
+
+var theItems = items;
+// console.dir(items);
+
+// _.each(theItems, function(i){
+//   // console.log(i);
+// })
+
+// var information = _.map(theItems, function(i){
+//   return "The Price is " + i.price
+// });
+
+var itemTemplate = $('#info-template').text();
+
+var renderTemplate = _.template(itemTemplate);
+
+_.each(theItems.results, function(i){
+  console.log(i);
+});
 
 }());
